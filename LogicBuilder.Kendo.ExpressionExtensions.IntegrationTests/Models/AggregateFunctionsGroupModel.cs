1b69cbfdd38c5cc95bb9823cf54167dab1c8c8ad
Isen Ng <3904043+isen-ng@users.noreply.github.com>
2020-03-13 11:57:18 +0800

    Remove files that depend on telerik so that integration tests can compile

--- LogicBuilder.Kendo.ExpressionExtensions.IntegrationTests/Models/AggregateFunctionsGroupModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using Kendo.Mvc.Infrastructure;
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace LogicBuilder.Kendo.ExpressionExtensions.IntegrationTests.Models
-{
-    public class AggregateFunctionsGroupModel<TModel> : AggregateFunctionsGroup { }
-}
